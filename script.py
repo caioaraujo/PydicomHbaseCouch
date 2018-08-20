f1398f45d74a7a90f51cbe0bb81b4882da36b237
Clayton RM <claytonrmendonca@gmail.com>
2018-08-19 21:14:09 -0300

    Saving base64 of image.

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
+import base64
 import json
 import os
 import sys
 
 import couchdb
-import pydicom
 import happybase
+import pydicom
+from pydicom.filebase import DicomBytesIO
 from thriftpy.transport import TTransportException
 
 
@@ -205,12 +207,17 @@ if __name__ == '__main__':
 
             print('Working in file:', file)
 
-            dicom_dataset = pydicom.dcmread(file)
+            with open(file, 'rb') as fp:
+                raw = DicomBytesIO(fp.read())
 
-            dicom_dataset_dict = __extract_dataset_to_dict(dicom_dict, dicom_dataset, file)
+                dicom_dataset = pydicom.dcmread(raw)
 
-            rowkey, column_family = __define_column_family(dicom_dataset)
+                compressed = sys.stdout.buffer.write(base64.b64encode(dicom_dataset.pixel_array.tobytes()))
 
-            insert_in_hbase(rowkey, column_family, json.dumps(dicom_dataset_dict))
+                dicom_dataset_dict = __extract_dataset_to_dict(dicom_dict, dicom_dataset, compressed)
 
-            insert_in_couchdb(rowkey, dicom_dataset)
+                rowkey, column_family = __define_column_family(dicom_dataset)
+
+                insert_in_hbase(rowkey, column_family, json.dumps(dicom_dataset_dict))
+
+                insert_in_couchdb(rowkey, dicom_dataset)
